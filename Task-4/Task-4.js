1dd06b1dbfc98f450391a69bb5799da197bdc5a2
Immernachtreich <ebrahimtohfafarosh11@gmail.com>
2022-10-13 23:59:49 +0530

    Added Task-4 files and renamed some previous files

---
+++ Task-4/Task-4.js
@@ -0,0 +1,12 @@
+const items = document.getElementsByClassName('list-group-item');
+
+console.log(items);
+console.log(items[1]);
+
+//Sharpener Code
+
+items[2].style.backgroundColor='green';
+
+for(let i = 0; i < items.length; i++) {
+    items[i].style.fontWeight='bold';
+}
